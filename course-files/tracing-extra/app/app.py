be5f7d91102678b5c3c1125cc6de8f8557cfbff8
jmeng <jmeng@datto.com>
2021-11-06 23:00:58 -0400

    fix issue

--- course-files/tracing-extra/app/app.py
+++ course-files/tracing-extra/app/app.py
@@ -39,16 +39,16 @@ def init_tracer(service):
 #starter code
 tracer = init_tracer('test-service')
 
-config = Config(
-    config={
-        'sampler':
-        {'type': 'const',
-         'param': 1},
-                        'logging': True,
-                        'reporter_batch_size': 1,},
-                        service_name="service")
-jaeger_tracer = config.initialize_tracer()
-tracing = FlaskTracing(jaeger_tracer, True, app)
+# config = Config(
+#     config={
+#         'sampler':
+#         {'type': 'const',
+#          'param': 1},
+#                         'logging': True,
+#                         'reporter_batch_size': 1,},
+#                         service_name="service")
+# jaeger_tracer = config.initialize_tracer()
+# tracing = FlaskTracing(jaeger_tracer, True, app)
 
 
 # not entirely sure but I believe there's a flask_opentracing.init_tracing() missing here
@@ -66,21 +66,23 @@ def hello_world():
 
 @app.route('/alpha')
 def alpha():
-    for i in range(100):
-        do_heavy_work() # removed the colon here since it caused a syntax error - not sure about its purpose?
-        if i % 100 == 99:
-            time.sleep(10)
+    with tracer.start_span('alpha') as span:
+        for i in range(100):
+            do_heavy_work() # removed the colon here since it caused a syntax error - not sure about its purpose?
+            if i % 100 == 99:
+                time.sleep(10)
     return 'This is the Alpha Endpoint!'
 
  
 @app.route('/beta')
 def beta():
-    r = requests.get("https://www.google.com/search?q=python")
-    dict = {}
-    for key, value in r.headers.items():
-        print(key, ":", value)
-        dict.update({key: value})
-    return jsonify(dict)      
+    with tracer.start_span('beta') as span:
+        r = requests.get("https://www.google.com/search?q=python")
+        dict = {}
+        for key, value in r.headers.items():
+            print(key, ":", value)
+            dict.update({key: value})
+        return jsonify(dict)
 
 
 
@@ -96,9 +98,6 @@ def writeredis():
         dict.update({key: value})
     rdb.mset(dict)    
     return jsonify(dict)      
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
